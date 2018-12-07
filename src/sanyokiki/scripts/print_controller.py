bed85ff8dff218c41862e192bc970976548f0f91
Igarashi <igarashi@bme.en.a.u-tokyo.ac.jp>
2018-12-07 19:29:41 +0900

    any changes

--- src/sanyokiki/scripts/print_controller.py
+++ src/sanyokiki/scripts/print_controller.py
@@ -5,7 +5,6 @@ import rospy
 import serial
 import sys
 import time
-import datetime
 import math
 
 from geometry_msgs.msg import Twist
@@ -18,17 +17,19 @@ StartByte_1 = "S"
 StartByte_2 = "T"
 ForwardBackward_Neutral = 512 # neutral value
 LeftRight_Neutral = 512       # neutral value
-EngineSpeed = "0A49"          # minimam speed
+EngineSpeed = "0E0E"          # minimam speed is 0A49
 EngineOn = "0000"             # engine off value, engine on value is "0080"
 AutonomousOff = "0000"
-MaxSpeed_Limit = "0041"
+MaxSpeed_Limit = "0050"
 CorrectionDataA = "0000"
 CorrectionDataB = "0000"
 LineFeed = chr(0x0d)
 CarriageReturn = chr(0x0a)
 CommandLength = 36            # command of sanyokiki weeder is 36 length.
 
-PowerAdjust = 320
+PowerAdjust = 400
+DiagonalAdjust = 110
+SleepConstant = 0.060 # second
 
 
 class controller():
@@ -38,34 +39,35 @@ class controller():
         self.cmd_vel_time = 0
         self.safetystop()
 
+        rospy.on_shutdown(self.shutdown)
         print "ready to controller"
         rospy.Subscriber('engine_onoff', Int8, self.sendbot)    # ROS callback function of engine command
         rospy.Subscriber('cmd_vel', Twist, self.sendbot_cmd)    # ROS callback function of cmd_vel
         rospy.Subscriber('human_proximity', HumanProximity, self.proximity)    # ROS callback function
-        rospy.spin()
+        #rospy.spin()
 
     def proximity(self, data):
         self.human_proximity_stamp = data.header.stamp.secs
         self.human_proximity_value = data.proximity_value
 
     def send_sanyocontroller(self, ForwardBackward, LeftRight, EngineOn):
-        ControlCommand = StartByte_1 +StartByte_2 +ForwardBackward +LeftRight +EngineSpeed +EngineOn +AutonomousOff +MaxSpeed_Limit +CorrectionDataA +CorrectionDataB +LineFeed +CarriageReturn
+        self.ControlCommand = StartByte_1 +StartByte_2 +ForwardBackward +LeftRight +EngineSpeed +EngineOn +AutonomousOff +MaxSpeed_Limit +CorrectionDataA +CorrectionDataB +LineFeed +CarriageReturn
 
-        now = []
         t1 = time.time()
         for i in range(CommandLength):
-            #t = datetime.datetime.now() #debug option
-            #now.append(t.microsecond) #debug option
-            #ser.flushInput()
-            #ser.write(ControlCommand[0+i:1+i])
-            #ser.flush()
+            #self.ser.write(self.ControlCommand[0+i:1+i])
+            #self.ser.flush()
             #print ControlCommand[0+i:1+i] #debug option
-            time.sleep(0.001) # need 1 msec sleep
-        print ControlCommand #debug option
-        #print now #debug option
+            time.sleep(0.0008) # need 0.8 msec sleep
 
-        #t2 = time.time()
-        #print (t2 - t1)*1000, "msec"
+        t2 = time.time()
+        sleep_time = SleepConstant - (t2 - t1)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        # debug option
+        #t3 = time.time()
+        #print (t3-t1)
+        #print self.ControlCommand
 
     def sendbot_cmd(self, new_cmd):
         Power_linear = 0
@@ -87,11 +89,11 @@ class controller():
         # Forward
         elif(new_cmd.linear.x > 0):
             Power_linear = int(PowerAdjust * math.fabs(new_cmd.linear.x))
-            Power_angular = int(PowerAdjust * math.fabs(new_cmd.angular.z))
-            if(new_cmd.angular.z > 0):
+            Power_angular = int(DiagonalAdjust * math.fabs(new_cmd.angular.z))
+            if(new_cmd.angular.z > 0): # diagonally forward left
                 self.ForwardBackward_value = ForwardBackward_Neutral +Power_linear
                 self.LeftRight_value = int(LeftRight_Neutral +Power_angular)
-            elif(new_cmd.angular.z < 0):
+            elif(new_cmd.angular.z < 0): # diagonally forward left
                 self.ForwardBackward_value = ForwardBackward_Neutral +Power_linear
                 self.LeftRight_value = int(LeftRight_Neutral -Power_angular)
             else:
@@ -100,11 +102,11 @@ class controller():
         # Backward
         elif(new_cmd.linear.x < 0):
             Power_linear = int(PowerAdjust * math.fabs(new_cmd.linear.x))
-            Power_angular = int(PowerAdjust * math.fabs(new_cmd.angular.z))
-            if(new_cmd.angular.z > 0):
+            Power_angular = int(DiagonalAdjust * math.fabs(new_cmd.angular.z))
+            if(new_cmd.angular.z > 0): # diagonally backward left
                 self.ForwardBackward_value = ForwardBackward_Neutral -Power_linear
                 self.LeftRight_value = int(LeftRight_Neutral +Power_angular)
-            elif(new_cmd.angular.z < 0):
+            elif(new_cmd.angular.z < 0): # diagonally backward right
                 self.ForwardBackward_value = ForwardBackward_Neutral -Power_linear
                 self.LeftRight_value = int(LeftRight_Neutral -Power_angular)
             else:
@@ -149,18 +151,25 @@ class controller():
                 print "safety stop by detecting /cmd_vel interruption"
 
         # statement of serial write
-        self.send_sanyocontroller(self.ForwardBackward_value, self.LeftRight_value, EngineOn)
+        self.send_sanyocontroller(str(self.ForwardBackward_value), str(self.LeftRight_value), EngineOn)
 
     def safetystop(self):
         self.ForwardBackward_value = '0' +hex(ForwardBackward_Neutral)[2:5]
         self.LeftRight_value = '0' +hex(LeftRight_Neutral)[2:5]
 
-def shutdown():
-    rospy.loginfo("sanyo controller was terminated")
-    #ser.close()
+    # loginfo loop of control command
+    def loop(self):
+        while not rospy.is_shutdown():
+            try:
+                rospy.loginfo(self.ControlCommand)
+            except AttributeError:
+                self.ControlCommand = None
+            rospy.sleep(0.5)
+
+    def shutdown(self):
+        rospy.loginfo("sanyo controller was terminated")
     
-# main
 if __name__ == '__main__':
     rospy.init_node('sanyo_controller')
-    rospy.on_shutdown(shutdown)
     c = controller()
+    c.loop()
