b2541e6433549541e65a4ef0b6e641d23ef074e0
sho <igarashi@bme.en.a.u-tokyo.ac.jp>
2018-12-21 02:01:13 +0900

    fix yaw problem, maybe

--- src/gnss_yaw/scripts/imu_sub.py
+++ src/gnss_yaw/scripts/imu_sub.py
@@ -13,10 +13,17 @@ from tf.transformations import euler_from_quaternion
 pub = rospy.Publisher('/gnss_imu', Odometry, queue_size = 10)
 
 def imu(imu_msg):
-
     imu_e = euler_from_quaternion((imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w))
     imu_yaw = imu_e[2]/np.pi *180
-    print imu_yaw
+    print "imu_", imu_yaw
+
+def gnss_imu(msg):
+    gnss_imu_e = euler_from_quaternion((msg.pose.pose.orientation.x, 
+                                        msg.pose.pose.orientation.y, 
+                                        msg.pose.pose.orientation.z, 
+                                        msg.pose.pose.orientation.w))
+    yaw = gnss_imu_e[2]/np.pi *180
+    print "gnss", yaw
 
 def shutdown():
     rospy.loginfo("imu_data_sub_node was terminated")
@@ -25,6 +32,7 @@ def listener():
     rospy.init_node('imu_data_sub_node')
     rospy.on_shutdown(shutdown)
     rospy.Subscriber('/imu/data', Imu, imu) # ROS callback function
+    rospy.Subscriber('/gnss_imu', Odometry, gnss_imu) # ROS callback function
     rospy.spin()
 
 if __name__ == '__main__':
